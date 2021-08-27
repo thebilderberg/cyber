a1e1b6075f2618a977ac543115ce629142d7ea14
thebilderberg <mygooglegirls@yandex.ru>
2021-08-27 19:27:55 +0300

    vol.2

--- alert.js
+++ alert.js
@@ -1,1 +1,47 @@
-alert( 'Добро пожаловать на тёмную сторону Луны!' );
+"use strict";
+
+
+function buttonClickOne() {
+  let clickOne = document.querySelector('.bottomClassOne');
+  clickOne.addEventListener('click', oneBotton);
+}
+
+
+function oneBotton() {
+  alert('Тут ничего нет');
+}
+
+
+function buttonClickTwo() {
+  let clickOne = document.querySelector('.bottomClassTwo');
+  clickOne.addEventListener('click', twoBotton);
+}
+function twoBotton() {
+  alert('Тут ничего нет');
+}
+
+
+function buttonClickThree() {
+  let clickOne = document.querySelector('.bottomClassThree');
+  clickOne.addEventListener('click', threeBotton);
+}
+function threeBotton() {
+  alert('Тут ничего нет');
+}
+
+
+function buttonClickFour() {
+  let clickOne = document.querySelector('.bottomClassFour');
+  clickOne.addEventListener('click', exitSms);
+}
+function exitSms() {
+  alert('Ты нащёл приключений на жопу, чумба!');
+}
+
+
+
+
+buttonClickOne();
+buttonClickTwo();
+buttonClickThree();
+buttonClickFour();
